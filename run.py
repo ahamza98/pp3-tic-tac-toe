08ffbcbdf6ed42d89ff40e5adbae0b4a4e1d7f22
ahamza98 <a_hamza98@hotmail.com>
2022-04-11 13:15:26 +0000

    add core functions

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# from colors import Color 
+
+# user = 0
+# ai = 0
+# winner = none
+
+# def welcome():
+# Welcome to a Tic Tac Toe
+
+# def menu() 
+# Option of Rules
+# Play Game
+# def rules ()
+
+# def play_game()
+# board
+# def display_board()
+
+# def switch_player()
+
+# def check_win()
+
+# def check_vertical()
+# def check_horizontal()
+# def check_diagnol()
+
+# def updateScores
+
+# def play_again()
+
+
+# def main()
